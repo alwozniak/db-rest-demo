2ae6b36dd32440a0347f30db621396c22b8e337c
Aleksandra Wozniak <aleksandra.k.wozniak@gmail.com>
2018-07-17 16:06:17 +0200

    Configuring database. Introducing AuditModel: a base class for other domain model classes.

--- src/main/java/com/alwozniak/example/dbrestdemo/DbRestDemoApplication.java
+++ src/main/java/com/alwozniak/example/dbrestdemo/DbRestDemoApplication.java
@@ -2,11 +2,13 @@ package com.alwozniak.example.dbrestdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DbRestDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DbRestDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DbRestDemoApplication.class, args);
+    }
 }
---
+++ src/main/java/com/alwozniak/example/dbrestdemo/domain/AuditModel.java
@@ -0,0 +1,45 @@
+package com.alwozniak.example.dbrestdemo.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Abstract base class for domain classes stored in the database.
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)  // For automatically populating updatedAt and createdAt.
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public abstract class AuditModel implements Serializable {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
