167c2fa0c5e13a59bc27f3ba73bb915cf710d2b6
Aleksandra Wozniak <aleksandra.k.wozniak@gmail.com>
2018-07-17 15:15:26 +0200

    Initial commit

---
+++ src/main/java/com/alwozniak/example/dbrestdemo/DbRestDemoApplication.java
@@ -0,0 +1,12 @@
+package com.alwozniak.example.dbrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbRestDemoApplication.class, args);
+	}
+}
