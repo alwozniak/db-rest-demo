39fad28046d92f04921c0b477edb7bbae0765c42
Aleksandra Wozniak <aleksandra.k.wozniak@gmail.com>
2018-07-17 16:19:37 +0200

    Repository interfaces

---
+++ src/main/java/com/alwozniak/example/dbrestdemo/repository/AnswerRepository.java
@@ -0,0 +1,12 @@
+package com.alwozniak.example.dbrestdemo.repository;
+
+import com.alwozniak.example.dbrestdemo.domain.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestionId(Long questionId);
+}
